969c4a20cd48dbd0f64f89139b60bd0290a35dcf
moaliyou <moaliyou47@gmail.com>
2023-08-29 20:53:55 +0300

    Fixed: Lemonade App generative images

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -17,8 +17,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,32 +65,100 @@ fun MakingLemonadeScreen(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ImageButtonWithText()
+        var stepCounter by remember {
+            mutableStateOf(1)
+        }
+
+        var squeezeCounter by remember {
+            mutableStateOf(0)
+        }
+
+        when (stepCounter) {
+            1 -> {
+                ImageButtonAndText(
+                    imageResourceId = R.drawable.lemon_tree,
+                    contentDescriptionId = R.string.lemon_tree_content_description,
+                    textResourceId = R.string.lemon_tree_label
+                ) {
+                    stepCounter++
+                    squeezeCounter = (2..4).random()
+                }
+            }
+            2 -> {
+                ImageButtonAndText(
+                    imageResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionId = R.string.glass_of_lemonade_content_description,
+                    textResourceId = R.string.glass_of_lemonade_label
+                ) {
+                    squeezeCounter--
+                    if (squeezeCounter == 0)
+                        stepCounter++
+                }
+            }
+            3 -> {
+                ImageButtonAndText(
+                    imageResourceId = R.drawable.lemon_drink,
+                    contentDescriptionId = R.string.lemon_content_description,
+                    textResourceId = R.string.lemon_label
+                ) {
+                    stepCounter++
+                }
+            }
+            4 -> {
+                ImageButtonAndText(
+                    imageResourceId = R.drawable.lemon_restart,
+                    contentDescriptionId = R.string.empty_glass_content_description,
+                    textResourceId = R.string.empty_glass_label
+                ) {
+                    stepCounter = 1
+                }
+            }
+        }
     }
 
 }
 
 @Composable
-fun ImageButtonWithText() {
-    ButtonWithImage()
-    Spacer(modifier = Modifier.height(16.dp))
+fun ImageButtonAndText(
+    imageResourceId: Int,
+    contentDescriptionId: Int,
+    textResourceId: Int,
+    onImageClick: () -> Unit
+
+) {
+    ButtonWithImage(imageResourceId, contentDescriptionId, onImageClick)
+    Spacer(modifier = Modifier.height(18.dp))
     Text(
-        text = stringResource(R.string.lemon_tree_label),
+        text = stringResource(textResourceId),
         fontSize = 18.sp
     )
 }
 
 @Composable
-fun ButtonWithImage() {
+fun ButtonWithImage(
+    painterValue: Int,
+    contentDescriptionId: Int,
+    onImageClick: () -> Unit
+
+) {
+
     Button(
         shape = RoundedCornerShape(52.dp),
         contentPadding = PaddingValues(24.dp),
         colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiaryContainer),
-        onClick = { /*TODO*/ }
+        onClick = onImageClick
     ) {
-        Image(
-            painter = painterResource(R.drawable.lemon_tree),
-            contentDescription = stringResource(R.string.lemon_tree_content_description)
+        GetImage(
+            painter = painterResource(painterValue),
+            contentDescription = stringResource(contentDescriptionId)
         )
     }
+}
+
+@Composable
+fun GetImage(painter: Painter, contentDescription: String?) {
+    Image(
+        painter = painter,
+        contentDescription = contentDescription
+    )
 }
